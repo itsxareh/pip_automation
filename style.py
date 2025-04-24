6f6eea6acd9c74dcbd505a3df00040fc9403a9dd
itsxareh <raytos.r.bsinfotech@gmail.com>
2025-04-24 08:45:57 +0800

    Debugged Fully Paid PTP

--- style.py
+++
@@ -1,9 +0,0 @@
-from streamlit_option_menu import option_menu
-
-selected = option_menu(
-    menu_title=None,
-    options=["Home", "Upload", "Settings"],
-    icons=["house", "cloud-upload", "gear"],
-    menu_icon="cast",
-    orientation="horizontal",
-)
