545b8c9ba6526346678e1efcfab072f323461ea7
itsxareh <raytos.r.bsinfotech@gmail.com>
2025-04-24 14:28:19 +0800

    Different UI

--- app.py
+++ app.py
@@ -1179,7 +1179,7 @@ def main():
             }
             div[data-baseweb] {
                 font-size: 12px;
-                height: auto;
+                line-height: 0;
             }
             div[data-testid="stToolbar"] {
                 display: none;
